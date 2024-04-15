98941fae6a9c60ff384af8f70d457511c939cd22
akash-panthri <akashpanthri2000@gmail.com>
2024-04-16 04:09:51 +0530

    Added models to models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.CharField(max_length=255)
+    id_user = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
+    profileimg = models.ImageField(upload_to='profile_images')
+    location = models.CharField(max_length=255)
