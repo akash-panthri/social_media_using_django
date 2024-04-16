25f270d886168f29e017a462efb97b0ccdaa9d99
akash-panthri <akashpanthri2000@gmail.com>
2024-04-17 04:04:29 +0530

    Added signup route

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    
+        return render(request, 'signup.html')
