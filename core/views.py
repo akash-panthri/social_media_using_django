48c4a089a77425a37aefdf09284dcbc75be0f51c
akash-panthri <akashpanthri2000@gmail.com>
2024-04-22 04:34:51 +0530

    Created upload feature

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile , Post
 
 # Create your views here.
 def index(request):
@@ -94,4 +94,14 @@ def signout(request):
     return redirect('signin')
 
 def upload(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
