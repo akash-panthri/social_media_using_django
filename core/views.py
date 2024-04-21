2d4ff8b8764ec556f425ef3e3098a50009d7a80a
akash-panthri <akashpanthri2000@gmail.com>
2024-04-22 04:12:49 +0530

    Added user_profile to index in views.py

--- core/views.py
+++ core/views.py
@@ -6,7 +6,9 @@ from .models import Profile
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html' , {'user_profile': user_profile})
 
 
 
